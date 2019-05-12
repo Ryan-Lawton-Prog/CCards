96802c27e09e38cc5ead99360632c18dbeb0e4fd
Ryan-Lawton-Prog <50411373+Ryan-Lawton-Prog@users.noreply.github.com>
2019-05-12 22:25:30 +1000

    Defining project files

--- cards.c
+++ cards.c
@@ -1,3 +1,1 @@
 #include "cards.h"
-
-
---
+++ cards.h
@@ -0,0 +1,9 @@
+#ifndef CARDS_H
+#define CARDS_H
+
+#include <stdio.h>  /*scanf, printf, fopen*/
+#include <string.h> /*strcmp, strcpy, strlen*/
+#include <stdlib.h> /*fprintf, fscanf, sscanf*/
+
+
+#endif
