c5a1557808462d8a2a433b4095a3bb5c26277cf4
Ryan-Lawton-Prog <50411373+Ryan-Lawton-Prog@users.noreply.github.com>
2019-05-12 23:25:57 +1000

    Created Card definitions
    Forming the linked list implementation of a card and deck system for data manipulation.

--- cards.c
+++ cards.c
@@ -1,1 +1,33 @@
 #include "cards.h"
+
+card_t card(char[] data){
+  card_t n = NULL;
+  n->data = data;
+  return n;
+}
+
+card_t card(char[] data, card_t * next){
+  card_t n = NULL;
+  n->data = data;
+  n->next = next;
+  return n;
+}
+
+char[] getData(card_t n){
+  return n->data;
+}
+
+char[] setData(char[] data, card_t * n){
+  return n->data = data;
+}
+
+card_t getNext(card_t n){
+  return n->next;
+}
+
+int getSize(card_t n){
+  if(n->next == NULL){
+    return 1;
+  }
+  return getSize(n->next) + 1;
+}
--- cards.h
+++ cards.h
@@ -4,6 +4,26 @@
 #include <stdio.h>  /*scanf, printf, fopen*/
 #include <string.h> /*strcmp, strcpy, strlen*/
 #include <stdlib.h> /*fprintf, fscanf, sscanf*/
+#include "linkedList.h"
 
+
+struct card{
+  char[] data;
+  struct card * next;
+};
+typedef card card_t
+
+struct deck{
+  card_t[] cards;
+  struct deck * next;
+};
+typedef deck deck_t
+
+card_t card(char[] data);
+card_t card(char[] data, card_t * next);
+char[] getData(card_t n);
+char[] setData(char[] data, card_t * n);
+card_t getNext(card_t n);
+int getSize(card_t n);
 
 #endif
