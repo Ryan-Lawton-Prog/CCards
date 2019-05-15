c1057d5d459df9872983fb662cdc94609bafa4d2
Ryan-Lawton-Prog <50411373+Ryan-Lawton-Prog@users.noreply.github.com>
2019-05-15 13:46:45 +1000

    Created basic menu system

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
+/*******************************************************************************
+ * 48430 Fundamentals of C Programming - Assignment 3
+ * Team: ?
+ * Team Members: 
+ *  Anto Mathews - 
+ *  Ayush Bhatia -  
+ *  Gordon Wang - 
+ *  Jenny Tran - 
+ *  Ryan Lawton - 12545341
+ * Date of submission: ?
+*******************************************************************************/
+
 #include "deck.h"
+#include "menu.h"
 
 int main(){
-
+  int menu = 0;
+  while(menu != -1){
+    /* prints menu and gets menu type */
+    print_menu();
+    scanf("%d",&menu);
+    switch(menu)
+    {
+      case -1:
+        break;
+      default:
+        printf("Invalid choice\n");
+        break;
+    }
+  }
   return 0;
 }
