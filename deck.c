1dad4ea3c6733e639b45b7748a772ee43ab6dada
Ryan-Lawton-Prog <50411373+Ryan-Lawton-Prog@users.noreply.github.com>
2019-05-18 20:20:43 +1000

    Added more deck stat funcs

--- deck.c
+++ deck.c
@@ -11,6 +11,10 @@ deck_t create_deck(){
   deck_t temp; 
   temp = (deck_t)malloc(sizeof(struct deck));
   strcpy(temp->name, "");
+  strcpy(temp->author, "");
+  strcpy(temp->owner, "");
+  temp->played = 0;
+  temp->accuracy = 0;
   temp->cards = NULL;
   temp->next = NULL;
   return temp;
@@ -23,10 +27,14 @@ deck_t create_deck(){
  * outputs:
  * - deck_t (Top of deck heap)
 *******************************************************************************/
-deck_t add_deck(deck_t head, const char name[], card_t cards){
+deck_t add_deck(deck_t head, const char name[], const char author[], const char owner[], int played, double accuracy, card_t cards){
   deck_t temp,p;
   temp = create_deck();
   strcpy(temp->name,name);
+  strcpy(temp->author,author);
+  strcpy(temp->owner,owner);
+  temp->played = played;
+  temp->accuracy = accuracy;
   temp->cards = cards;
   if(!strcmp(head->name, "")){
     head = temp; 
@@ -53,29 +61,6 @@ void get_name(deck_t n, char name[]){
 }
 
 /*******************************************************************************
- * get cards from the deck 'n' and return the top of the card heap
- * inputs:
- * - deck_t
- * outputs:
- * - card_t
-*******************************************************************************/
-card_t get_cards(deck_t n){
-  return n->cards;
-}
-
-/*******************************************************************************
- * set the name and cards parameter of a given deck 'n'
- * inputs:
- * - deck_t, string (name), card_t (top of card heap)
- * outputs:
- * - none
-*******************************************************************************/
-void set_deck(deck_t n, const char name[], card_t cards){
-  strcpy(n->name, name);
-  n->cards = cards;
-}
-
-/*******************************************************************************
  * get name in position 'pos' from the
  * sequence of decks from the top of the stack 'n'
  * inputs:
@@ -98,6 +83,17 @@ void get_name_at(deck_t n, int pos, char name[]){
 }
 
 /*******************************************************************************
+ * get cards from the deck 'n' and return the top of the card heap
+ * inputs:
+ * - deck_t
+ * outputs:
+ * - card_t
+*******************************************************************************/
+card_t get_cards(deck_t n){
+  return n->cards;
+}
+
+/*******************************************************************************
  * get cards in position 'pos' from the
  * sequence of decks from the top of the stack 'n'
  * inputs:
@@ -111,11 +107,23 @@ card_t get_cards_at(deck_t n, int pos){
   int i;
   for(i = 0; i < get_deck_size(n); i++){
     if(i == pos){
-      return n->cards;
+      return temp->cards;
     }
     temp = temp->next;
   }
   return NULL;
+}
+
+/*******************************************************************************
+ * set the name and cards parameter of a given deck 'n'
+ * inputs:
+ * - deck_t, string (name), card_t (top of card heap)
+ * outputs:
+ * - none
+*******************************************************************************/
+void set_deck(deck_t n, const char name[], card_t cards){
+  strcpy(n->name, name);
+  n->cards = cards;
 }
 
 /*******************************************************************************
@@ -141,14 +149,171 @@ void set_deck_at(deck_t n, int pos, const char name[], card_t cards){
 }
 
 /*******************************************************************************
- * get next deck in the linked deck list
+ * get times played from the deck 'n'
  * inputs:
  * - deck_t
  * outputs:
- * - deck_t (next deck)
+ * - int
 *******************************************************************************/
-deck_t get_next_deck(deck_t n){
-  return n->next;
+int get_played(deck_t n){
+  return n->played;
+}
+
+/*******************************************************************************
+ * get times played in position 'pos' from the
+ * sequence of decks starting at the top of the stack 'n'
+ * inputs:
+ * - deck_t, int
+ * outputs:
+ * - int
+*******************************************************************************/
+int get_times_played_at(deck_t n, int pos){
+  deck_t temp;
+  temp = n;
+  int i;
+  for(i = 0; i < get_deck_size(n); i++){
+    if(i == pos){
+      return temp->played;
+    }
+    temp = temp->next;
+  }
+  return -1;
+}
+
+/*******************************************************************************
+ * get accuracy from the deck 'n'
+ * inputs:
+ * - deck_t
+ * outputs:
+ * - double
+*******************************************************************************/
+double get_accuracy(deck_t n){
+  return n->accuracy;
+}
+
+/*******************************************************************************
+ * get accuracy in position 'pos' from the
+ * sequence of decks starting at the top of the stack 'n'
+ * inputs:
+ * - deck_t, int
+ * outputs:
+ * - double
+*******************************************************************************/
+double get_accuracy_at(deck_t n, int pos){
+  deck_t temp;
+  temp = n;
+  int i;
+  for(i = 0; i < get_deck_size(n); i++){
+    if(i == pos){
+      return temp->accuracy;
+    }
+    temp = temp->next;
+  }
+  return -1;
+}
+
+/*******************************************************************************
+ * get author from the deck 'n'
+ * inputs:
+ * - deck_t
+ * outputs:
+ * - none
+*******************************************************************************/
+void get_author(deck_t n, char author[]){
+  strcpy(author, n->author);
+}
+
+/*******************************************************************************
+ * get author in position 'pos' from the
+ * sequence of decks starting at the top of the stack 'n'
+ * inputs:
+ * - deck_t, int, string (author)
+ * outputs:
+ * - none
+*******************************************************************************/
+void get_author_at(deck_t n, int pos, char author[]){
+  deck_t temp;
+  temp = n;
+  int i;
+  for(i = 0; i < get_deck_size(n); i++){
+    if(i == pos){
+      strcpy(author, temp->author);
+      return;
+    }
+    temp = temp->next;
+  }
+  strcpy(author, "invalid index");
+}
+
+/*******************************************************************************
+ * get owner from the deck 'n'
+ * inputs:
+ * - deck_t
+ * outputs:
+ * - none
+*******************************************************************************/
+void get_owner(deck_t n, char owner[]){
+  strcpy(owner, n->owner);
+}
+
+/*******************************************************************************
+ * get owner in position 'pos' from the
+ * sequence of decks starting at the top of the stack 'n'
+ * inputs:
+ * - deck_t, int, string (owner)
+ * outputs:
+ * - none
+*******************************************************************************/
+void get_owner_at(deck_t n, int pos, char owner[]){
+  deck_t temp;
+  temp = n;
+  int i;
+  for(i = 0; i < get_deck_size(n); i++){
+    if(i == pos){
+      strcpy(owner, temp->owner);
+      return;
+    }
+    temp = temp->next;
+  }
+  strcpy(owner, "invalid index");
+}
+
+/*******************************************************************************
+ * Updates the accuracy of a deck after a new game has been played
+ * inputs:
+ * - deck_t, int
+ * outputs:
+ * - none
+*******************************************************************************/
+void update_stats(deck_t n, int correct){
+  n->accuracy = ((n->accuracy * n->played) + 
+    (correct / get_size(n->cards))) / 
+    (n->played + 1);
+  n->played++;
+}
+
+/*******************************************************************************
+ * Updates the accuracy of a deck at position 'pos' from the
+ * sequence of decks starting at the top of the stack 'n'
+ * inputs:
+ * - deck_t, int, int
+ * outputs:
+ * - none
+*******************************************************************************/
+void update_stats_at(deck_t n, int pos, int correct){
+  deck_t temp;
+  temp = n;
+  int i;
+  for(i = 0; i < get_deck_size(n); i++){
+    if(i == pos){
+      temp->accuracy = ((temp->accuracy * temp->played) + 
+        (correct / get_size(temp->cards))) / 
+        (temp->played + 1);
+      temp->played++;
+      return;
+    }
+    temp = temp->next;
+  }
 }
 
 /*******************************************************************************
@@ -163,4 +328,15 @@ int get_deck_size(deck_t n){
     return 1;
   }
   return get_deck_size(n->next) + 1;
+}
+
+/*******************************************************************************
+ * get next deck in the linked deck list
+ * inputs:
+ * - deck_t
+ * outputs:
+ * - deck_t (next deck)
+*******************************************************************************/
+deck_t get_next_deck(deck_t n){
+  return n->next;
 }
--- deck.h
+++ deck.h
@@ -4,15 +4,20 @@
 #include "cards.h"
 
 #define MAX_DECK_NAME_LENGTH 50
+#define MAX_AUTHOR_LENGTH 20
+#define MAX_OWNER_NAME_LENGTH 50
 
 /*******************************************************************************
  * deck struct - used as a linked list for storing each deck without having
  * to set re-adjust the size of a deck array or setting a maximum
 *******************************************************************************/
 struct deck{
-  char name[MAX_DECK_NAME_LENGTH+1];
   card_t cards;
-  /* add stat information */
+  char name[MAX_DECK_NAME_LENGTH+1];
+  char author[MAX_AUTHOR_LENGTH+1];
+  char owner[MAX_OWNER_NAME_LENGTH+1];
+  int played;
+  double accuracy;
   struct deck * next;
 };
 typedef struct deck * deck_t;
@@ -22,14 +27,24 @@ typedef struct deck * deck_t;
  * Function prototypes
 *******************************************************************************/
 deck_t create_deck();
-deck_t add_deck(deck_t, const char[], card_t);
+deck_t add_deck(deck_t, const char[], const char[], const char[], int, double, card_t);
 void get_name(deck_t, char[]);
-card_t get_cards(deck_t);
-void set_deck(deck_t, const char[], card_t);
 void get_name_at(deck_t, int, char[]);
+card_t get_cards(deck_t);
 card_t get_cards_at(deck_t, int);
+void set_deck(deck_t, const char[], card_t);
 void set_deck_at(deck_t, int, const char[], card_t);
-deck_t get_next_deck(deck_t);
+int get_played(deck_t);
+int get_played_at(deck_t, int);
+double get_accuracy(deck_t);
+double get_accuracy_at(deck_t, int);
+void get_author(deck_t, char[]);
+void get_author_at(deck_t, int, char[]);
+void get_owner(deck_t, char[]);
+void get_owner_at(deck_t, int, char[]);
+void update_stats(deck_t, int);
+void update_stats_at(deck_t, int, int);
 int get_deck_size(deck_t);
+deck_t get_next_deck(deck_t);
 
 #endif
