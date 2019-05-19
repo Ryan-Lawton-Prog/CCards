4009973ed9fe1ae4a5b2124f0c59ad47f6b0d554
Ryan-Lawton-Prog <50411373+Ryan-Lawton-Prog@users.noreply.github.com>
2019-05-19 22:38:59 +1000

    Added save and load methods
    * Community decks

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 
 #define MAX_USERNAME_LENGTH 20
 #define MAX_PASSWORD_LENGTH 20
+#define DB_COMMUNITY_DECKS "DB_COMMUNITY_DECKS"
 
 /*******************************************************************************
  * Function prototypes
@@ -26,7 +27,9 @@ void view_decks();
 void create_a_deck();
 void view_community_decks();
 void view_user_stats();
-void login(deck_t*, deck_t*, user_t*);
+void login(deck_t, deck_t, user_t*);
+deck_t load_community_decks();
+void save_community_decks(deck_t);
 
 /*******************************************************************************
  * Main
@@ -35,7 +38,7 @@ int main(){
     int menu = 0;
     int logged_in = 1; /*once loggin has been implemented set to 0*/
     deck_t decks = create_deck();
-    deck_t community_decks = create_deck();
+    deck_t community_decks = load_community_decks();
     user_t user = create_user();
     while(menu != -1){
         if(logged_in){
@@ -53,6 +56,9 @@ int main(){
                     test_add_card();
                     break;
                 case 2:
+                    if(strcmp(community_decks->name, "") > 0){
+                        save_community_decks(community_decks);
+                    }
                     break;
                 case 3:
                     break;
@@ -65,14 +71,92 @@ int main(){
                     break;
             }
         }else{
-            login(&decks, &community_decks, &user);
+            login(decks, community_decks, &user);
         }
-        /* prints menu and gets menu type */
     }
     return 0;
 }
 
-void login(deck_t*deck, deck_t*community_deck, user_t*user){
+void login(deck_t deck, deck_t community_deck, user_t*user){
     print_login();
 }
 
+deck_t load_community_decks(){
+    deck_t final_deck = create_deck();
+    deck_t temp_deck = create_deck();
+    card_t final_card = create_card();
+    card_t temp_card = create_card();
+    FILE * filep;
+    filep = fopen(DB_COMMUNITY_DECKS, "r");
+    if(filep == NULL){
+        printf("Could not find Community Deck DB\n");
+        return NULL;
+    }
+    int eof = 1;
+    int reading_cards = 0;
+    while(eof){
+        char temp_l[100] = {'\0'};
+        char temp_r[100] = {'\0'};
+        eof = fscanf(filep, "%s %[^\n]", temp_l, temp_r) != EOF;
+        if(!eof){
+            break;
+        }
+        if(!reading_cards){
+            if(!strcmp(temp_l, "name")){
+                strcpy(temp_deck->name,temp_r);
+            }else if(!strcmp(temp_l, "author")){
+                strcpy(temp_deck->author,temp_r);
+            }else if(!strcmp(temp_l, "owner")){
+                strcpy(temp_deck->owner,temp_r);
+            }else if(!strcmp(temp_l, "played")){
+                sscanf(temp_r, "%d", &temp_deck->played);
+            }else if(!strcmp(temp_l, "accuracy")){
+                sscanf(temp_r, "%lf", &temp_deck->accuracy);
+                reading_cards = 1;
+            }
+        }else{
+            if(!strcmp(temp_l, "end")){
+                reading_cards = 0;
+                final_deck = add_deck(final_deck, temp_deck->name, temp_deck->author, temp_deck->owner, temp_deck->played, temp_deck->accuracy, final_card);
+                temp_card = create_card();
+                final_card = create_card();
+                temp_deck = create_deck();
+            }else if(!strcmp(temp_l, "question")){
+                strcpy(temp_card->question,temp_r);
+            }else if(!strcmp(temp_l, "answer")){
+                strcpy(temp_card->answer,temp_r);
+                final_card = add_card(final_card, temp_card->question, temp_card->answer);
+                temp_card = create_card();
+            }
+        }
+    }
+    fclose(filep);
+    return final_deck;
+}
+
+void save_community_decks(deck_t deck){
+    deck_t current_deck = deck;
+    card_t current_card = deck->cards;
+    FILE * filep;
+    filep = fopen(DB_COMMUNITY_DECKS, "w");
+    int i;
+    for(i = 0; i < get_deck_size(deck); i++){
+        if(i > 0){
+            current_deck = current_deck->next;
+            current_card = current_deck->cards;
+        }
+        fprintf(filep, "%s %s\n", "name", current_deck->name);
+        fprintf(filep, "%s %s\n", "author", current_deck->author);
+        fprintf(filep, "%s %s\n", "owner", current_deck->owner);
+        fprintf(filep, "%s %d\n", "played", current_deck->played);
+        fprintf(filep, "%s %lf\n", "accuracy", current_deck->accuracy);
+        int j;
+        for(j = 0; j < get_size(current_deck->cards); j++){
+            fprintf(filep, "%s %s\n", "question", current_card->question);
+            fprintf(filep, "%s %s\n", "answer", current_card->answer);
+            current_card = current_card->next;
+        }
+        fprintf(filep, "%s %s\n", "end", "deck");
+    }
+    fclose(filep);
+}
