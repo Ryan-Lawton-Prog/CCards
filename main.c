28c8e424026d35da5ac7db17cf80746c62308dbe
jenny-tran1 <50400801+jenny-tran1@users.noreply.github.com>
2019-05-22 13:43:56 +1000

    Added Create a deck in main.c

--- main.c
+++ main.c
@@ -17,15 +17,14 @@
 
 #define MAX_USERNAME_LENGTH 20
 #define MAX_PASSWORD_LENGTH 20
-#define MAX_INPUT_LENGTH 100
 
 /*******************************************************************************
  * Function prototypes
 *******************************************************************************/
 
 void view_decks();
-void create_a_deck();
-void view_community_decks(deck_t, deck_t, user_t);
+void create_a_deck(deck_t);
+void view_community_decks();
 void view_user_stats();
 void login(deck_t, deck_t, user_t*);
 
@@ -60,14 +59,14 @@ int main(){
                     }
                     break;
                 case 3:
+                    create_a_deck(decks);
                     break;
                 case 4:
-                    view_community_decks(community_decks, decks, user);
                     break;
                 case 5:
                     break;
                 default:
-                    print_invalid_command();
+                    printf("Invalid choice\n");
                     break;
             }
         }else{
@@ -81,33 +80,71 @@ void login(deck_t deck, deck_t community_deck, user_t*user){
     print_login();
 }
 
-void view_community_decks(deck_t community_decks, deck_t decks, user_t user){
-    /*printing community decks*/
-    while(1){
-        clear_screen();
-        deck_t temp = community_decks;
-        char input[100];
-        int i;
-        print_community_decks(community_decks);
-        while((getchar()) != '\n');
-        scanf("%[^\n]", input);
-        if(!strcmp(input, "exit")){
-            return;
-        }else{
-            temp = community_decks;
-            int size = get_deck_size(community_decks);
-            for(i = 0; i < size; i++){
-                if(i > 0){
-                    temp = temp->next;
-                }
-                if(!strcmp(input, temp->name)){
-                    decks = add_deck(decks, temp->name, temp->author, user.username, 0, 0, 0, temp->cards);
-                    update_deck_db(get_last_deck(decks));
-                    printf("Added: \"%s\" to you deck collection\n", input);
-                    return;
-                }
+void create_a_deck(deck_t decks){
+    card_t cards = create_card();
+    char name[MAX_DECK_NAME_LENGTH];
+    char question[MAX_CARD_QUESTION_LENGTH];
+    char answer[MAX_CARD_ANSWER_LENGTH];
+    char user_input[10];
+    int is_public = 0;
+    int choice = 0;
+    int i;
+
+    do{
+        printf("What do you want to name your deck?\n");
+        scanf("%s", name);
+    } while (name[0] == '\0');
+
+
+    while(choice != -1){
+
+        printf("Write your question card:\n");
+        scanf("%s", question);
+
+        printf("Write your answer card:\n");
+        scanf("%s", answer);
+
+        cards = add_card(cards, question, answer);
+
+        while(choice != -1){
+            printf("Do you want to add more cards? " 
+                   "Type 'yes' to continue or 'no' to stop\n");
+            scanf("%s", user_input);
+            
+            if(strcmp(user_input, "no") == 0){
+                choice = -1;
+            } else if (strcmp(user_input, "yes") == 0){
+                break;
+            } else {
+                printf("Invalid choice\n");
             }
         }
-        print_invalid_deck();
+    } 
+
+    while(is_public == 0){
+        printf("Do you want make this deck public or private? "
+               "Type 1 for public or 2 for private.\n");
+        scanf("%d", &is_public);
+
+        if(is_public == 1 || is_public == 2){
+            break;
+        } else {
+            printf("Invalid choice\n");
+            is_public = 0;
+        }
+    }
+ 
+    decks = add_deck(decks, name, "Jenny", "Tran", is_public, 0, 1.0, cards);
+    
+    /*Testing by printing values*/
+    printf("Name: %s\n", decks->name);
+    printf("Author: %s\n", decks->author);
+    printf("Owner: %s\n", decks->owner);
+    printf("Is public: %d\n", decks->is_public);
+
+    for(i=0; get_size(decks->cards); i++){
+        printf("Card %d Question: %s\n", i+1, decks->cards->question);
+        printf("Card %d Answer: %s\n", i+1, decks->cards->answer);
+        decks->cards = decks->cards->next;
     }
 }
